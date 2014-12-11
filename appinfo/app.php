9d2c7cc4119018684b05476fba494da540abadd2
Thomas Müller <thomas.mueller@tmit.eu>
2014-12-11 15:05:59 +0100

    dummy app to show some missing dependencies

---
+++ appinfo/app.php
@@ -0,0 +1,11 @@
+<?php
+
+
+OCP\App::addNavigationEntry(array(
+  'id' => 'all_missing_dependencies',
+  'order' => 1,
+//  'href' => OCP\Util::linkToRoute( 'mail.page.index' ),
+//  'icon' => OCP\Util::imagePath( 'mail', 'mail.svg' ),
+  'name' => 'all_missing_dependencies',
+));
+
